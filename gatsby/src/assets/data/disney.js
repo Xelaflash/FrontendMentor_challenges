9f39ed6ac702793fad1656c2368a98e938867368
Alexandre Gros <xelaflash@gmail.com>
2021-08-31 10:13:31 -0400

    movies card done

--- gatsby/src/assets/data/disney.js
+++ gatsby/src/assets/data/disney.js
@@ -1,27 +1,90 @@
 export const movies = [
   {
-    name: "falcon and the winter soldier",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "slider2.PNG",
+    name: 'falcon and the winter soldier',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+    image: 'slider2.PNG',
   },
   {
-    name: "loki",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "slider1.PNG",
+    name: 'loki',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+    image: 'slider1.PNG',
   },
   {
-    name: "wanda vision",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "slider3.PNG",
+    name: 'wanda vision',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+    image: 'slider3.PNG',
   },
   {
-    name: "raya and the last dragon",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "slider4.PNG",
+    name: 'raya and the last dragon',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+    image: 'slider4.PNG',
   },
   {
-    name: "luca",
-    des: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!",
-    image: "/slider5.PNG",
+    name: 'luca',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit porro et veniam excepturi, eaque voluptatem impedit nulla laboriosam facilis ut laboriosam libero!',
+    image: '/slider5.PNG',
+  },
+];
+
+export const moviesCard = [
+  {
+    name: 'Loki',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 1.png',
+  },
+  {
+    name: 'Mulan',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 2.png',
+  },
+  {
+    name: 'Falcon and the winter soldier',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 3.png',
+  },
+  {
+    name: 'Avengers',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 4.png',
+  },
+  {
+    name: 'Thor Ragnarok',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 5.png',
+  },
+  {
+    name: 'The Avengers',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 6.png',
+  },
+  {
+    name: 'Pirate of the Caribbean',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 7.png',
+  },
+  {
+    name: 'Soul',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 8.png',
+  },
+  {
+    name: 'Raya',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 9.png',
+  },
+  {
+    name: 'Luca',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 10.png',
+  },
+  {
+    name: 'Ford vs Ferrari',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 11.png',
+  },
+  {
+    name: 'Dark phoenix',
+    des: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. ',
+    image: 'poster 12.png',
   },
 ];
--- gatsby/src/pages/disneyClone.js
+++ gatsby/src/pages/disneyClone.js
@@ -1,13 +1,14 @@
-import React, { useEffect } from "react";
-import "../styles/disney.scss";
-import Img from "gatsby-image";
-import { graphql, useStaticQuery } from "gatsby";
-import { movies } from "../assets/data/disney";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Slider from "react-slick";
+/* eslint-disable jsx-a11y/media-has-caption */
+import React, { useEffect } from 'react';
+import '../styles/disney.scss';
+import Img from 'gatsby-image';
+import { graphql, useStaticQuery } from 'gatsby';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { movies, moviesCard } from '../assets/data/disney';
 
-export default function disneyClone() {
+export default function DisneyClone() {
   const data = useStaticQuery(graphql`
     query {
       logo: file(relativePath: { eq: "images/disney/logo.png" }) {
@@ -28,20 +29,38 @@ export default function disneyClone() {
     autoplay: true,
     autoplaySpeed: 5000,
     speed: 2500,
-    cssEase: "linear",
+    cssEase: 'linear',
   };
 
-  /// video cards
   useEffect(() => {
-    const videoCards = [...document.querySelectorAll(".video-card")];
+    // video cards
+    const videoCards = [...document.querySelectorAll('.video-card')];
     videoCards.forEach((item) => {
-      item.addEventListener("mouseover", () => {
-        let video = item.children[1];
+      item.addEventListener('mouseover', () => {
+        const video = item.children[1];
         video.play();
       });
-      item.addEventListener("mouseleave", () => {
-        let video = item.children[1];
+      item.addEventListener('mouseleave', () => {
+        const video = item.children[1];
         video.pause();
+      });
+    });
+
+    // card sliders
+    const cardContainers = [...document.querySelectorAll('.card-container')];
+    const preBtns = [...document.querySelectorAll('.pre-btn')];
+    const nxtBtns = [...document.querySelectorAll('.nxt-btn')];
+
+    cardContainers.forEach((item, i) => {
+      const containerDimensions = item.getBoundingClientRect();
+      const containerWidth = containerDimensions.width;
+
+      nxtBtns[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth - 200;
+      });
+
+      preBtns[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth + 200;
       });
     });
   });
@@ -92,49 +111,94 @@ export default function disneyClone() {
       </div>
       <div className="video-card-container">
         <div className="video-card">
-          <img src="disney.PNG" className="video-card-image" alt="" />
+          <img
+            src="disney.PNG"
+            className="video-card-image"
+            alt="disney logo"
+          />
           <video
             src="videos/disney/disney.mp4"
             mute="true"
             loop
             className="card-video"
-          ></video>
+          />
         </div>
         <div className="video-card">
-          <img src="pixar.PNG" className="video-card-image" alt="" />
+          <img src="pixar.PNG" className="video-card-image" alt="pixar logo" />
           <video
             src="videos/disney/pixar.mp4"
             mute="true"
             loop
             className="card-video"
-          ></video>
+          />
         </div>
         <div className="video-card">
-          <img src="marvel.PNG" className="video-card-image" alt="" />
+          <img
+            src="marvel.PNG"
+            className="video-card-image"
+            alt="marvel logo"
+          />
           <video
             src="videos/disney/marvel.mp4"
             mute="true"
             loop
             className="card-video"
-          ></video>
+          />
         </div>
         <div className="video-card">
-          <img src="star-wars.PNG" className="video-card-image" alt="" />
+          <img
+            src="star-wars.PNG"
+            className="video-card-image"
+            alt="star wars logo"
+          />
           <video
             src="videos/disney/star-war.mp4"
             mute="true"
             loop
             className="card-video"
-          ></video>
+          />
         </div>
         <div className="video-card">
-          <img src="geographic.PNG" className="video-card-image" alt="" />
+          <img
+            src="geographic.PNG"
+            className="video-card-image"
+            alt="National geographic logo"
+          />
           <video
             src="videos/disney/geographic.mp4"
             mute="true"
             loop
             className="card-video"
-          ></video>
+          />
+        </div>
+      </div>
+      <div className="movies-container">
+        <h2 className="movies-title">recommended for you</h2>
+        <div className="movies-list">
+          <button className="pre-btn" type="button">
+            <img src="pre.png" alt="prev button icon" />
+          </button>
+          <button className="nxt-btn" type="button">
+            <img src="nxt.png" alt="next button icon" />
+          </button>
+          <div className="card-container">
+            {moviesCard.map((card) => (
+              <div className="card" key={card.name}>
+                <img
+                  src={`${card.image}`}
+                  className="card-img"
+                  alt={card.name}
+                />
+                <div className="card-body">
+                  <h2 className="name">{card.name}</h2>
+                  <h6 className="des">{card.des}</h6>
+                  <button className="watchlist-btn" type="button">
+                    Add to watchlist
+                  </button>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
