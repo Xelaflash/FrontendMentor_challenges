bd4b70a82532969ecbd7466f12b7e97de5d89ad8
Alexandre Gros <xelaflash@gmail.com>
2021-09-20 12:46:34 -0400

    finish challenge

--- gatsby/src/pages/markdownPreviewer.js
+++ gatsby/src/pages/markdownPreviewer.js
@@ -1,67 +1,86 @@
 import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import ReactMarkdown from 'react-markdown';
-import remarkGfm from 'remark-gfm'
+import remarkGfm from 'remark-gfm';
 import Layout from '../components/Layout';
 import addExternalScript from '../utils/addExternalScript.js';
 
 const MarkdownPreviewerStyles = styled.div`
   color: var(--white);
+  .markdown_editor,
+  .markdown_previewer {
+    width: fit-content;
+    border-radius: 5px;
+    margin: 20px auto;
+    filter: drop-shadow(3px 2px 6px rgb(255 255 255 / 50%));
+    textarea {
+      resize: none;
+    }
+  }
+
   // MARKDOWN STYLES
-#preview {
-  padding: 0 1rem;
+  #preview {
+    white-space: pre-line;
+    ul {
+      list-style-type: disc;
+      list-style-position: inside;
+    }
+    ol {
+      list-style-type: decimal;
+      list-style-position: inside;
+    }
+    ul ul,
+    ol ul {
+      list-style-type: circle;
+      list-style-position: inside;
+      margin-left: 15px;
+    }
+    ol ol,
+    ul ol {
+      list-style-type: lower-latin;
+      list-style-position: inside;
+      margin-left: 15px;
+    }
+    blockquote {
+      border-left: 3px solid #224b4b;
+      color: #224b4b;
+      padding-left: 5px;
+      margin-left: 25px;
+    }
+    p > code {
+      padding: 3px;
+    }
 
-  blockquote {
-    border-left: 3px solid #224b4b;
-    color: #224b4b;
-    padding-left: 5px;
-    margin-left: 25px;
-  }
+    code {
+      background: white;
+      font-size: 1.2rem;
+      font-weight: bold;
+    }
 
-  > p > code {
-    padding: 3px;
-  }
+    pre {
+      display: block;
+      overflow: auto;
+      background: white;
+      padding: 5px;
+      line-height: 1.2;
+    }
 
-  code {
-    background: white;
-    font-size: 0.875rem;
-    font-weight: bold;
-    color:red
-  }
+    table {
+      border-collapse: collapse;
+    }
 
-  pre {
-    display: block;
-    overflow: auto;
-    background: white;
-    padding: 5px;
-    line-height: 1.2;
+    td,
+    th {
+      border: 2px solid #224b4b;
+      padding-left: 5px;
+      padding-right: 5px;
+    }
+    img {
+      display: block;
+      max-width: 90%;
+      margin: 2rem auto;
+    }
   }
-
-  h1 {
-    border-bottom: 2px solid red;
-  }
-
-  h2 {
-    border-bottom: 1px solid blue;
-  }
-
-  table {
-    border-collapse: collapse;
-  }
-
-  td,
-  th {
-    border: 2px solid white;
-    padding-left: 5px;
-    padding-right: 5px;
-  }
-
-  img {
-    display: block;
-    max-width: 90%;
-    margin: 2rem auto;
-  }
-}
 `;
 
 export default function MarkdownPreviewer() {
@@ -107,9 +126,8 @@ And here. | Okay. | I think we get it.
 1. Use just 1s if you want!
 1. And last but not least, let's not forget embedded images:
 
-![freeCodeCamp Logo](https://cdn.freecodecamp.org/testable-projects-fcc/images/fcc_secondary.svg)
+![Random Image](https://picsum.photos/400/600)
 `;
-
 
   const [markdownText, setMarkdownText] = useState(placeholder);
   useEffect(() => {
@@ -121,28 +139,43 @@ And here. | Okay. | I think we get it.
   return (
     <Layout>
       <MarkdownPreviewerStyles>
-        <div className="markdown_editor m-15">
-          <h2 className="text-6xl font-semibold tracking-wide my-6">
+        <div className="markdown_editor m-15 my-20">
+          <h2 className="rounded-md text-6xl font-semibold tracking-wide bg-green-700 p-6 text-center ">
             Markdown Editor
           </h2>
           <textarea
             name="markdown"
             id="editor"
-            className="rounded-md text-gray-700 placeholder-gray-700::placeholder text-black"
+            className="rounded-md text-gray-700 text-white bg-green-400"
             cols="80"
             rows="20"
             placeholder="Type your markdown text here"
             onChange={(e) => setMarkdownText(e.target.value)}
             defaultValue={placeholder}
-          ></textarea>
+          />
         </div>
-        <div className="markdown_previewer">
-          <h2 className="text-6xl font-semibold tracking-wide my-6">
+        <hr />
+        <div className="markdown_previewer my-20 m-15">
+          <h2 className="rounded-md text-6xl font-semibold tracking-wide bg-green-700 p-6 text-center">
             Markdown Previewer
           </h2>
-          <div id="preview">
-            <ReactMarkdown>{markdownText}</ReactMarkdown>
+          <div
+            id="preview"
+            className="rounded-md text-gray-700 text-white bg-green-400 p-10"
+          >
+            <ReactMarkdown
+              children={markdownText}
+              remarkPlugins={[remarkGfm]}
+            />
           </div>
+        </div>
+        <div
+          className="disclaimer text-red-700 text-center
+      "
+        >
+          Last test (#8) not passing because i did not used the markdown lib
+          recommanded by FCC. React-markdown pkg does not have an options for
+          line breaks.
         </div>
       </MarkdownPreviewerStyles>
     </Layout>
